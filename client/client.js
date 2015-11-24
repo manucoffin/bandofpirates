a836bcc058215e9619e3a6da9c2f02b3a0826651
Manuel <manuel.coffin@gmail.com>
2015-11-24 16:08:28 +0000

    map features
    implementation of movements on the map, physics and events

--- client/client.js
+++ client/client.js
@@ -1,16 +1,71 @@
-// counter starts at 0
-Session.setDefault('counter', 0);
+Template.game.rendered = function() {
 
-Template.hello.helpers({
-  counter: function () {
-    return Session.get('counter');
-  }
-});
+	var game = new Phaser.Game(
+		800, // size of the canvas created
+		600,
+		Phaser.CANVAS,
+		'game-view', // div in which the canvas is appened
+		{ 
+			preload: preload,
+			create: create,
+			update: update
+		}
+	);
 
-Template.hello.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    // Session.set('counter', Session.get('counter') + 1);
-    Meteor.myfunctions.sayHello();
-  }
-});
+	var map;
+	var layer;
+	var player;
+	var dest;
+	var s;
+
+	function preload(){
+	
+		game.load.tilemap('worldTileMap', 'assets/worldMap.json', null, Phaser.Tilemap.TILED_JSON);
+    	game.load.image('tiles', 'assets/tiles/super_mario.png');
+    	game.load.image('player','assets/sprites/guybrush_sprite.png');
+
+	}
+
+	function create() {
+
+	    game.stage.backgroundColor = '#787878';
+	    map = game.add.tilemap('worldTileMap');
+	    map.addTilesetImage('SuperMarioTileset', 'tiles');
+	    layer = map.createLayer('Background');
+	    layer.resizeWorld();
+
+	    game.world.setBounds(0, 0, 1600, 1600);
+	    game.physics.startSystem(Phaser.Physics.ARCADE);
+	    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+    	
+
+	    game.camera.follow(player);
+
+	    player.anchor.setTo(0.5, 0.5);
+
+	    //  Enable Arcade Physics for the player
+	    game.physics.enable(player, Phaser.Physics.ARCADE);
+
+	    //  Tell it we don't want physics to manage the rotation
+	    player.body.allowRotation = false;
+	    player.body.move = false;
+
+	    game.input.onDown.add(moveBoat, this);
+
+	}
+
+	function update(){
+
+	}
+
+
+	function moveBoat(pointer) {
+		//  300 = 300 pixels per second = the speed the sprite will move at, regardless of the distance it has to travel
+		var duration = (game.physics.arcade.distanceToPointer(player, pointer) / 500) * 1000;
+		tween = game.add.tween(player).to( {x: pointer.worldX, y: pointer.worldY}, duration, Phaser.Easing.Linear.None);
+
+	    tween.start();
+
+	}
+
+}
